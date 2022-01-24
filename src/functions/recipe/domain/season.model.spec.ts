f90eedba147a322e21248e1f876ba2ff51b86be9
Michael Vlasaty <mvlasaty23@gmail.com>
2022-01-25 00:24:49 +0100

    feat: impl season from

--- src/functions/recipe/domain/season.model.spec.ts
+++ src/functions/recipe/domain/season.model.spec.ts
@@ -10,4 +10,55 @@ describe('Season', () => {
       expect(() => Season.of(null)).toThrow(AssertionError);
     });
   });
+  describe('from', () => {
+    [
+      { day: new Date('2021-01-01'), expected: Season.of('winter') },
+      { day: new Date('2021-01-31'), expected: Season.of('winter') },
+      { day: new Date('2021-02-01'), expected: Season.of('winter') },
+      { day: new Date('2021-02-28'), expected: Season.of('winter') },
+      { day: new Date('2021-03-01'), expected: Season.of('winter') },
+      { day: new Date('2021-03-19'), expected: Season.of('winter') },
+      { day: new Date('2021-03-20'), expected: Season.of('spring') },
+      { day: new Date('2021-03-30'), expected: Season.of('spring') },
+      { day: new Date('2021-04-01'), expected: Season.of('spring') },
+      { day: new Date('2021-04-31'), expected: Season.of('spring') },
+      { day: new Date('2021-05-01'), expected: Season.of('spring') },
+      { day: new Date('2021-05-30'), expected: Season.of('spring') },
+      { day: new Date('2021-06-01'), expected: Season.of('spring') },
+      { day: new Date('2021-06-20'), expected: Season.of('spring') },
+      { day: new Date('2021-06-21'), expected: Season.of('summer') },
+      { day: new Date('2021-06-31'), expected: Season.of('summer') },
+      { day: new Date('2021-07-01'), expected: Season.of('summer') },
+      { day: new Date('2021-07-30'), expected: Season.of('summer') },
+      { day: new Date('2021-08-01'), expected: Season.of('summer') },
+      { day: new Date('2021-08-31'), expected: Season.of('summer') },
+      { day: new Date('2021-09-01'), expected: Season.of('summer') },
+      { day: new Date('2021-09-21'), expected: Season.of('summer') },
+      { day: new Date('2021-09-22'), expected: Season.of('autumn') },
+      { day: new Date('2021-09-30'), expected: Season.of('autumn') },
+      { day: new Date('2021-10-01'), expected: Season.of('autumn') },
+      { day: new Date('2021-10-31'), expected: Season.of('autumn') },
+      { day: new Date('2021-11-01'), expected: Season.of('autumn') },
+      { day: new Date('2021-11-30'), expected: Season.of('autumn') },
+      { day: new Date('2021-12-01'), expected: Season.of('autumn') },
+      { day: new Date('2021-12-20'), expected: Season.of('autumn') },
+      { day: new Date('2021-12-21'), expected: Season.of('winter') },
+      { day: new Date('2021-12-31'), expected: Season.of('winter') },
+    ].forEach((testCase) => {
+      it(`should return ${testCase.expected.value} for ${testCase.day.toISOString()}`, () => {
+        expect(Season.from(testCase.day)).toEqual(testCase.expected);
+      });
+    });
+  });
+  describe('equals', () => {
+    [
+      { actual: Season.of('autumn'), that: Season.of('autumn'), expected: true },
+      { actual: Season.of('autumn'), that: Season.of('summer'), expected: false },
+      { actual: Season.of('autumn'), that: null, expected: false },
+      { actual: Season.of('autumn'), that: Season.of('all'), expected: true },
+    ].forEach(({ actual, that, expected }) => {
+      it(`should return ${expected.toString()} for ${actual.value} == ${that ? that.value : 'null'}`, () =>
+        expect(actual.equals(that)).toEqual(expected));
+    });
+  });
 });
--- src/functions/recipe/domain/season.model.ts
+++ src/functions/recipe/domain/season.model.ts
@@ -1,12 +1,39 @@
 import { ok as assertOk } from 'assert';
 
-export type SeasonKeys = 'spring' | 'summer' | 'autumn' | 'winter';
+export type SeasonKeys = 'spring' | 'summer' | 'autumn' | 'winter' | 'all';
 
 export class Season {
   private constructor(public value: SeasonKeys) {}
   public static of(value: SeasonKeys): Season {
     assertOk(!!value, 'Season should not be null');
     return new Season(value);
+  }
+  public static from(day: Date): Season {
+    function isAfterOrSame(date: Date, monthDay: number, month: number): boolean {
+      return date.getMonth() + 1 > month || (date.getMonth() + 1 === month && date.getDate() >= monthDay);
+    }
+    function isBefore(date: Date, monthDay: number, month: number): boolean {
+      return date.getMonth() + 1 < month || (date.getMonth() + 1 === month && date.getDate() < monthDay);
+    }
+    if (isAfterOrSame(day, 21, 12) || isBefore(day, 20, 3)) {
+      return Season.of('winter');
+    } else if (isAfterOrSame(day, 22, 9)) {
+      return Season.of('autumn');
+    } else if (isAfterOrSame(day, 21, 6)) {
+      return Season.of('summer');
+    } else {
+      return Season.of('spring');
+    }
+  }
+
+  public equals(that: Season): boolean {
+    if (!that || !that.value) {
+      return false;
+    }
+    if (that.value === 'all') {
+      return true;
+    }
+    return this.value === that.value;
   }
 }
 
@@ -15,4 +42,5 @@ export const seasons: { [k in SeasonKeys]: Season } = {
   summer: Season.of('summer'),
   autumn: Season.of('autumn'),
   winter: Season.of('winter'),
+  all: Season.of('all'),
 } as const;
